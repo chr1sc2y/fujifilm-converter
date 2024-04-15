2534db1a03eac99b5e09fe07f1f72c4c43a5e3e2
Providence <zintrulcre@gmail.com>
2024-04-15 19:33:36 +1000

    feat: archive logic

--- main.py
+++ main.py
@@ -3,27 +3,28 @@ import os
 
 def convert(dir):
     make = "FUJIFILM"
-    model = "GFX100S"
-    camera = "Fujifilm GFX 100S"
+    model = "GFX100II"
+    camera = "Fujifilm GFX 100 II"
     command = f'exiftool -make="{make}" -model="{model}" -uniquecameramodel="{camera}" '
     command += dir
     print(command)
     os.system(command)
 
 
-def rename_all_files(dir):
-    output_dir = dir + "/exported_dng/"
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+def archive_originals(dir):
+    archive_dir = dir + "/archived/"
+    if not os.path.exists(archive_dir):
+        os.makedirs(archive_dir)
     for filename in os.listdir(dir):
         if filename.endswith(".dng_original"):
             original_path = os.path.join(dir, filename)
-            output_filename = filename.replace(".dng_original", ".dng")
-            output_path = os.path.join(output_dir, output_filename)
-            os.rename(original_path, output_path)
-            print(f"Renamed: {original_path} -> {output_path}")
+            archive_file = filename.replace(".dng_original", ".dng")
+            archive_path = os.path.join(archive_dir, archive_file)
+            os.rename(original_path, archive_path)
+            print(f"Renamed: {original_path} -> {archive_path}")
+
 
 if __name__ == "__main__":
     dir = input("Please input the dir: ")
     convert(dir)
-    rename_all_files(dir)
+    archive_originals(dir)
